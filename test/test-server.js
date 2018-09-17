14dd1f7f262b3a2e4e5ac9439612e9a9c212c834
Eduardas Verba <eduardas.v@hotmail.com>
2018-09-17 16:03:53 +0100

    blog removed

--- test/test-server.js
+++ test/test-server.js
@@ -8,42 +8,36 @@ chai.use(chaiHttp);
 
 describe('Homepage', function() {
   it('should display the homepage at / GET', function(done) {
-    chai.request(server)
-      .get('/')
-      .end(function(err, res){
-        res.should.have.status(200);
-        done();
-      });
+    chai.request(server).get('/').end(function(err, res) {
+      res.should.have.status(200);
+      done();
+    });
   });
   it('should contain the word Sparta at / GET', function(done) {
-    chai.request(server)
-      .get('/')
-      .end(function(err, res){
-        res.text.should.contain('Sparta')
-        done();
-      });
+    chai.request(server).get('/').end(function(err, res) {
+      res.text.should.contain('Sparta')
+      done();
+    });
   });
 });
 
-describe('Blog', function() {
-  it('should display the list of posts at /posts GET', function(done) {
-    chai.request(server)
-      .get('/posts')
-      .end(function(err, res){
-        res.should.have.status(200);
-        done();
-      });
-  });
-});
+// describe('Blog', function() {
+//   it('should display the list of posts at /posts GET', function(done) {
+//     chai.request(server)
+//       .get('/posts')
+//       .end(function(err, res){
+//         res.should.have.status(200);
+//         done();
+//       });
+//   });
+// });
 
 describe('Fibonacci', function() {
   it('should display the correct fibonacci value at /fibonacci/10 GET', function(done) {
-    chai.request(server)
-      .get('/fibonacci/10')
-      .end(function(err, res){
-        res.should.have.status(200);
-        res.text.should.contain('55');
-        done();
-      });
+    chai.request(server).get('/fibonacci/10').end(function(err, res) {
+      res.should.have.status(200);
+      res.text.should.contain('55');
+      done();
+    });
   });
 });
